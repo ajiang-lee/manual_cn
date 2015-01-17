ad1d9abd76b1fb1d47a11bf95894aabf8fa00a4f
ajiang-lee <ajiang_lee@hotmail.com>
2015-01-17 18:28:33 +0800

    complete 3.3.3 to 3.3.5

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 // 模板引擎（使用JADE）
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.set('x-powered-by', false);
+app.set('x-powered-by', true);
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
