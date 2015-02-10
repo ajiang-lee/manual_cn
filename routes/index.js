0c7ac97fb2ff095fb7177b615f7e8e60432fe4c8
ajiang_lee@hotmail.com <ajiang_453500>
2015-02-10 10:51:30 +0800

    完成【关于站点】、【更新历史】页面

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,18 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+/**
+ * <b>描述：</b>返回关于我们页面
+ * */
+router.get('/aboutUs', function(req, res) {
+    res.render('aboutUs');
+});
+
+/**
+ * <b>描述：</b>返回站点的更新记录页面
+ * */
+router.get('/history', function(req, res) {
+    res.render('history');
+});
+
 module.exports = router;
